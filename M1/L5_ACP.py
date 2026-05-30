1a71594bab93d93d050baac8793f89e26e281b72
pycode <devpython549@gmail.com>
2026-05-30 15:24:01 +0530

    lesson 5 done M1

---
+++ M1/L5_ACP.py
@@ -0,0 +1,12 @@
+# Assignment: Summer times
+# Create a program using conditional statements
+
+print("Welcome to the Summer Times program!")
+temperature = float(input("Enter the current temperature in Celsius: "))
+
+if temperature > 25:
+    print("It's hot outside! It feels like summer.")
+    print("Perfect weather for swimming and ice cream!")
+else:
+    print("It's not that hot today.")
+    print("You might want to wear a light jacket or stay cozy indoors.")
