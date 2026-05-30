c747fda20d34c9c65b7bb4f140546ff0fc621ead
pycode <devpython549@gmail.com>
2026-05-30 15:21:50 +0530

    lesson 4 completed

---
+++ M1/L4_ACP.py
@@ -0,0 +1,11 @@
+# Assignment: Square root
+# Create a program to calculate the square root
+
+# Taking input from the user
+num = float(input("Enter a number to find its square root: "))
+
+# Calculating the square root using the exponentiation operator (**)
+square_root = num ** 0.5
+
+# Displaying the result
+print("The square root of", num, "is", square_root)
