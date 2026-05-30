d9680ea33f7aee94996e55d1bce2847605de688e
pycode <devpython549@gmail.com>
2026-05-30 15:11:46 +0530

    Lesson 2 complete

---
+++ M1/L2_ACP.py
@@ -0,0 +1,16 @@
+# Assignment: Storing birthdays
+# Write a program to store birthday dates inside 5 different variables
+
+# Storing 5 different birthday dates in variables
+birthday_1 = "15th January"
+birthday_2 = "4th March"
+birthday_3 = "22nd July"
+birthday_4 = "10th October"
+birthday_5 = "30th November"
+
+# Printing out the birthdays
+print("Friend 1's birthday is on:", birthday_1)
+print("Friend 2's birthday is on:", birthday_2)
+print("Friend 3's birthday is on:", birthday_3)
+print("Friend 4's birthday is on:", birthday_4)
+print("Friend 5's birthday is on:", birthday_5)
